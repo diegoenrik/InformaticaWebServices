243ff546ecf85a307c5876b64c187a0d1801d92c
diegoenrik <diegoenrik@gmail.com>
2013-11-18 23:20:59 -0500

    Versión inicial
    Conexión con base de datos SQLServer 2012

---
+++ Informatica/Informatica/DataBase/NumeroService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Informatica.DataBase {
+    public class NumeroService:IDisposable {
+
+        NumeroDbDataContext numeroDb;
+
+        public NumeroService() {
+            numeroDb = new NumeroDbDataContext();
+        }
+
+        /// <summary>
+        /// Genera la cantidad solicitada de números aleatorios en el rango de signed smallint.
+        /// Pendiente 
+        /// </summary>
+        /// <param name="nombre">nombre de usuario</param>
+        /// <param name="hash">identificador hash</param>
+        /// <param name="cantidad">cantidad de números a generar</param>
+        public void GeneraAleatorios(string nombre, string hash, int cantidad) {
+            Random rand = new Random();
+            do {
+                int generar = 0;
+                if (cantidad > 10000) {
+                    generar = 10000;
+                    cantidad -= 10000;
+                } else {
+                    generar = cantidad;
+                    cantidad = 0;
+                }
+                List<Numero> numeros = new List<Numero>();
+                for (int i = 0; i < generar; i++) {
+                    numeros.Add(new Numero() {
+                        hash_user = hash,
+                        name = nombre,
+                        num = rand.Next(-32768, 32676)
+                    });
+                }
+                numeroDb.Numeros.InsertAllOnSubmit(numeros);
+                numeroDb.SubmitChanges();       //Almacena en listas de 10000
+            } while (cantidad > 0);
+        }
+
+        public void AlmacenaNumeros(string nombre, string hash, List<int> nums) {
+            List<Numero> numeros = new List<Numero>();
+            foreach (int num in nums) {
+                numeros.Add(new Numero() {
+                    hash_user = hash,
+                    name = nombre,
+                    num = num
+                });
+            }
+            numeroDb.Numeros.InsertAllOnSubmit(numeros);
+            numeroDb.SubmitChanges();       //Almacena en listas de 10000
+        }
+
+        public void ModificarEstado(string hash, string name, int estado) {
+            EstadoUsuario ea = numeroDb.EstadoUsuarios.FirstOrDefault(p => p.hash_user == hash && p.name == name);
+            if (ea != null) {
+                ea.status = 1;
+            } else {
+                ea = new EstadoUsuario() {
+                    hash_user = hash,
+                    name = name,
+                    status = estado
+                };
+                numeroDb.EstadoUsuarios.InsertOnSubmit(ea);
+            }
+            numeroDb.SubmitChanges();
+        }
+
+        public EstadoUsuario VerificarEstado(string hash, string name) {
+            return numeroDb.EstadoUsuarios.FirstOrDefault(p => p.hash_user == hash && p.name == name);
+        }
+
+        public int ObtenerMayorAlmacenado(string hash, string name) { 
+            int max=(from num in numeroDb.Numeros 
+                         where num.hash_user==hash && num.name==name
+                         group num by new {num.hash_user,num.name} into order
+                         select order.Max(p=>p.num)).SingleOrDefault<int>();
+            return max;
+        }
+
+        public void LimpiarListaUsuario(string hash, string name) { 
+            List<Numero> numeros= numeroDb.Numeros.Where(p => p.hash_user == hash && p.name == name).ToList();
+            numeroDb.Numeros.DeleteAllOnSubmit(numeros);
+        }
+
+        #region IDisposable Members
+
+        public void Dispose() {
+            numeroDb.Dispose();
+        }
+
+        #endregion
+    }
+}
---
+++ Informatica/Informatica/Service1.asmx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Informatica.DataBase;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Informatica {
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://informatica.lynxsurver.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService {
+
+        [WebMethod]
+        public int ObtenerMayorArray(string nombre_usuario, int[] numeros) {
+            if (numeros == null || numeros.Length == 0) {
+                return 0;
+            }
+            if (numeros.Length > 10000) {
+                numeros = numeros.Take(10000).ToArray();
+            }
+            return numeros.Max();
+        }
+
+        [WebMethod]
+        public int ObtenerMayorLista(string nombre_usuario, List<int> numeros) {
+            if (numeros == null || numeros.Count == 0) {
+                return 0;
+            }
+            if (numeros.Count > 10000) {
+                numeros = numeros.Take(10000).ToList();
+            }
+            return numeros.Max();
+        }
+
+        [WebMethod]
+        public int GenerarNumeros(string nombre_usuario, int cantidad) {
+            string ip = HttpContext.Current.Request.UserHostAddress;
+            string browser = HttpContext.Current.Request.UserAgent;
+            NumeroService numeroServices = new NumeroService();
+            string hash=CalculateMD5Hash(ip + browser + nombre_usuario);
+            numeroServices.ModificarEstado(hash, nombre_usuario, 1);        //procesando
+            numeroServices.GeneraAleatorios(nombre_usuario, hash, cantidad);
+            numeroServices.ModificarEstado(hash, nombre_usuario, 0);        //libre
+            return cantidad;
+        }
+
+        [WebMethod]
+        public int AlmacenarNumeros(string nombre_usuario, int[] numeros) {
+            if (numeros == null || numeros.Length == 0) {
+                return 0;
+            }
+            if (numeros.Length > 10000) {
+                numeros = numeros.Take(10000).ToArray();
+            }
+            string ip = HttpContext.Current.Request.UserHostAddress;
+            string browser = HttpContext.Current.Request.UserAgent;
+            string hash = CalculateMD5Hash(ip + browser + nombre_usuario);
+            NumeroService numeroServices = new NumeroService();
+            numeroServices.AlmacenaNumeros(nombre_usuario, hash, numeros.ToList());
+            return numeros.Length;
+        }
+
+        [WebMethod]
+        public string ObtenerMayor(string nombre_usuario) {
+            string ip = HttpContext.Current.Request.UserHostAddress;
+            string browser = HttpContext.Current.Request.UserAgent;
+            string hash = CalculateMD5Hash(ip + browser + nombre_usuario);
+            NumeroService numeroServices = new NumeroService();
+            EstadoUsuario ea = numeroServices.VerificarEstado(hash, nombre_usuario);
+            if (ea != null && ea.status == 1) {     //procesando
+                return "PROCESANDO";
+            }
+            int mayor = numeroServices.ObtenerMayorAlmacenado(hash, nombre_usuario);
+            return mayor.ToString();
+        }
+
+        [WebMethod]
+        public string LimpiarLista(string nombre_usuario) {
+            string ip = HttpContext.Current.Request.UserHostAddress;
+            string browser = HttpContext.Current.Request.UserAgent;
+            string hash = CalculateMD5Hash(ip + browser + nombre_usuario);
+            NumeroService numeroServices = new NumeroService();
+            numeroServices.LimpiarListaUsuario(hash, nombre_usuario);
+            return "OK";
+        }
+
+        private string CalculateMD5Hash(string input) {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++) {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+    }
+}
